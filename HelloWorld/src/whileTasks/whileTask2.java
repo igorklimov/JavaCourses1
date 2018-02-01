42b02ee3f67c6b4270d30674b6878797cdc3bb64
igorklimov <igor.klimov092@gmail.com>
2018-02-02 00:53:02 +0200

    while Task 2

--- HelloWorld/src/whileTasks/whileTask2.java
+++ HelloWorld/src/whileTasks/whileTask2.java
@@ -1,4 +1,5 @@
 package whileTasks;
+import java.util.Scanner;
 
 public class whileTask2 {
     public static void main (String [] args) {
@@ -7,6 +8,17 @@ public class whileTask2 {
         // Если первое число меньше второго то при помощи цикла while
         // вывести на консоль числа от первого до второго.
 
+        Scanner numberInput = new Scanner(System.in);
+        System.out.println("Enter two numbers ");
+        int number1 = numberInput.nextInt();
+        int number2 = numberInput.nextInt();
+
+        if (number1 < number2) {
+            while (number1 <= number2) {
+                System.out.println(number1);
+                number1++;
+            }
+        }
 
     }
 }
