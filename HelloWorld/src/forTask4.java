d0066b485fbc33afe6e713c5246d319726ff6a9f
igorklimov <igor.klimov092@gmail.com>
2018-02-02 00:22:24 +0200

    for Task 4

---
+++ HelloWorld/src/forTask4.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class forTask4 {
+    public static void main (String [] args) {
+        // Написать консольную программу, которая запрашивает у пользователя два целых
+        // числа и если первое число меньше второго то считает сумму целых чисел от первого до второго включительно.
+
+
+        int number1, number2, sum = 0;
+        Scanner numberInput = new Scanner(System.in);
+        System.out.println("Enter two numbers: ");
+        number1 = numberInput.nextInt();
+        number2 = numberInput.nextInt();
+        
+        if (number1 < number2) {
+            for(int i=number1; i<=number2; i++) {
+                sum += i;
+            }
+            System.out.println(sum);
+        }
+        else {
+            System.out.println("Nope.");
+        }
+
+    }
+}
