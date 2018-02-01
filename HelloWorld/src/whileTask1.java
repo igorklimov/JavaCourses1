6f03143acdb3782d6484ad7058834f0a3887e857
igorklimov <igor.klimov092@gmail.com>
2018-02-02 00:36:21 +0200

    While tasks

---
+++ HelloWorld/src/whileTask1.java
@@ -0,0 +1,13 @@
+public class whileTask1 {
+    public static void main (String [] args) {
+        // Написать консольную программу, которая при помощи цикла while печатает на консоли числа от 0 до 100.
+
+        int i = 0;
+
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
